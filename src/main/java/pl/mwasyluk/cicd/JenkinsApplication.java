d1b2904012713654bd242032f7fecf479f30ded4
mWasyluk <marekwasyluk@wp.pl>
2024-06-24 14:45:12 +0200

    short-lived task

--- src/main/java/pl/mwasyluk/cicd/JenkinsApplication.java
+++ src/main/java/pl/mwasyluk/cicd/JenkinsApplication.java
@@ -1,20 +1,7 @@
 package pl.mwasyluk.cicd;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-@SpringBootApplication
-@RestController
 public class JenkinsApplication {
-
 	public static void main(String[] args) {
-		SpringApplication.run(JenkinsApplication.class, args);
-	}
-
-	@GetMapping("/")
-	public String helloWorld(){
-		return "Hello, world!";
+		System.out.println("Hello, world!");
 	}
 }
