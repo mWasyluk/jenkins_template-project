6d43bb6f020ad3d2b71e98f1501eb7d81454fc0f
mWasyluk <marekwasyluk@wp.pl>
2024-06-24 11:07:42 +0200

    Initial commit

---
+++ src/test/java/pl/mwasyluk/cicd/JenkinsApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.mwasyluk.cicd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
