72a55fc0e373b48b75097e209a46a196af1592ac
MWasyluk <75240925+mWasyluk@users.noreply.github.com>
2024-06-27 14:44:57 +0200

    ref: verify jenkins agent polling

--- src/main/java/pl/mwasyluk/cicd/JenkinsApplication.java
+++ src/main/java/pl/mwasyluk/cicd/JenkinsApplication.java
@@ -2,6 +2,6 @@ package pl.mwasyluk.cicd;
 
 public class JenkinsApplication {
 	public static void main(String[] args) {
-		System.out.println("Hello, world!");
+		System.out.println("Hello, world! Auto polling works just fine.");
 	}
 }
